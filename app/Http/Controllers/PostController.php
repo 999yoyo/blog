043c5841edc58b19cbac9b92e7594db192ca034e
999yoyo <yoh.ogawa.8f@stu.hosei.ac.jp>
2022-05-30 22:14:16 +0900

    correct commitment

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -2,14 +2,42 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
 use App\Post;
+use App\Http\Requests\PostRequests;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
     public function index(Post $post)
+    {
+        return view('posts/index')->with(['posts' => $post->getPaginateByLimit()]);  
+    }
+    public function show(Post $post)
+    {
+        return view('posts/show')->with(['post' => $post]);
+    }
+    public function create()
+    {
+        return view('posts/create');
+    }
+    public function store(Request $request, Post $post)
+    {
+        $input = $request['post'];
+        $post->fill($input)->save();
+        return redirect('/posts/' . $post->id);
+    }
+    public function edit(Post $post)
+    {
+    return view('posts/edit')->with(['post' => $post]);
+    }
+    public function update(PostRequest $request, Post $post)
 {
-    return $post->get();
+    $input_post = $request['post'];
+    $post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
 }
-    //
+
 }
+?>
